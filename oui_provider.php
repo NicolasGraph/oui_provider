78e44aaa9ed8a0fbab828a3634b47537922f33c9
Nicolas Morand <contact@nicolasmorand.com>
2018-06-27 13:13:01 +0200

    Is set anymway

--- oui_provider.php
+++ oui_provider.php
@@ -605,10 +605,10 @@ namespace Oui\Player {
                     if ($ratio) {
                         if ($width) {
                             preg_match("/(\D+)/", $width, $unit);
-                            $height = (int) $width / $aspect . (isset($unit[0]) ? $unit[0] : '');
+                            $height = (int) $width / $aspect . ($unit ? $unit[0] : '');
                         } else {
                             preg_match("/(\D+)/", $height, $unit);
-                            $width = (int) $height * $aspect . (isset($unit[0]) ? $unit[0] : '');
+                            $width = (int) $height * $aspect . ($unit ? $unit[0] : '');
                         }
                     } else {
                         trigger_error(gtxt('undefined_player_size'));
